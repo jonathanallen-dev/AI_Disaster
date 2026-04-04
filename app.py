0c56abc915c971afb3e2203a442c2776b8de596f
jonathanallen-dev <jonathan.allen@gmail.com>
2026-04-03 21:32:08 -0700

    Update app.py
    set ports correctly

--- app.py
+++ app.py
@@ -795,4 +795,5 @@ def live_earthquake_map():
 
 # --- Run App ---
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
